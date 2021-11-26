c58f531916d40b1c2a8d4e0b26018873b11b3907
Euro <no>
2021-11-25 21:39:10 -0800

    remove: -e, better error messages

--- term-write.py
+++ term-write.py
@@ -9,13 +9,16 @@ import sys, os
 import fcntl, termios
 import argparse
 
+def raiseError(errorType, error, exitCode=1):
+    print(f'\033[31m[{errorType}]\033[0m: {error}')
+    exit(exitCode)
+
 if sys.platform not in ("linux", "linux1", "linux2"):
-    raise OSError("This program only runs on linux")
+    raiseError("OSError", "This program only runs on linux")
 
 argumentParser = argparse.ArgumentParser(description="execute a command on 1 terminal from another")
 argumentParser.add_argument("terminal", metavar="T", type=int, nargs=1)
 argumentParser.add_argument("command",  metavar="C", type=str, nargs="+")
-argumentParser.add_argument("-e", "--escape", action="store_const", const=True, default=False)
 argumentParser.add_argument("-n", "--new-line", action="store_const", const=False, default=True)
 
 args = argumentParser.parse_args()
@@ -23,12 +26,10 @@ args = argumentParser.parse_args()
 fd = os.open(f"/dev/pts/{args.terminal[0]}", os.O_RDWR)
 cmd = " ".join(args.command)
 userId = os.getuid()
-if userId != 0: raise PermissionError("Must run as root")
+if userId != 0: raiseError("PermissionError", "Must be run as root")
 #in order to execute as command, each byte must be sent with termios.TIOCSTI
 #add the new line to simulate enter
 if args.new_line: cmd += "\n"
-if args.escape:
-    cmd = cmd.replace("\\n", "\n").replace("\\t", "\t").replace("\\e", "\033")
 for char in cmd:
    fcntl.ioctl(fd, termios.TIOCSTI, char)
 os.close(fd)
