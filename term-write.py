2329fd370ef43593d2ae00c376575981d756661e
Euro <no>
2021-11-25 21:20:17 -0800

    feat: -n, -e, infinite command args

--- term-write.py
+++ term-write.py
@@ -14,16 +14,21 @@ if sys.platform not in ("linux", "linux1", "linux2"):
 
 argumentParser = argparse.ArgumentParser(description="execute a command on 1 terminal from another")
 argumentParser.add_argument("terminal", metavar="T", type=int, nargs=1)
-argumentParser.add_argument("command",  metavar="C", type=str, nargs=1)
+argumentParser.add_argument("command",  metavar="C", type=str, nargs="+")
+argumentParser.add_argument("-e", "--escape", action="store_const", const=True, default=False)
+argumentParser.add_argument("-n", "--new-line", action="store_const", const=False, default=True)
 
 args = argumentParser.parse_args()
 
 fd = os.open(f"/dev/pts/{args.terminal[0]}", os.O_RDWR)
-cmd = args.command[0]
+cmd = " ".join(args.command)
 userId = os.getuid()
 if userId != 0: raise PermissionError("Must run as root")
 #in order to execute as command, each byte must be sent with termios.TIOCSTI
 #add the new line to simulate enter
-for char in cmd + '\n':
+if args.new_line: cmd += "\n"
+if args.escape:
+    cmd = cmd.replace("\\n", "\n").replace("\\t", "\t").replace("\\e", "\033")
+for char in cmd:
    fcntl.ioctl(fd, termios.TIOCSTI, char)
 os.close(fd)
